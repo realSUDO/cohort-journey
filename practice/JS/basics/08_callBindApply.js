7d7e736d705d0f23c3e9c154289febb32ecee435
Sumit Vishwkarma <justmultiplythinks@gmail.com>
2026-04-19 13:16:12 +0530

    Add : missing files in basics/

--- practice/JS/basics/08_callBindApply.js
+++ practice/JS/basics/08_callBindApply.js
@@ -1,8 +1,22 @@
-String.prototype.moymoy = function(){
-	return this.toUpperCase()	
+// String.prototype.moymoy = function(){
+// 	return this.toUpperCase()
+// }
+// let mystr = "something"
+//
+//
+//
+// console.log(mystr.moymoy()) // intentional.. lower for upper lol
+
+function multiply(a, b) {
+	console.log(this.name);
+	return a - b;
 }
-let mystr = "something"
+let myObj = {
+	name: "moy moy",
+};
 
+// Create a new function that always multiplies by 2
+const double = multiply.bind(myObj, 2);
 
-
-console.log(mystr.moymoy()) // intentional.. lower for upper lol
+console.log(double(5)); // Output: 10
+console.log(double(10)); // Output: 20
--- practice/JS/basics/DOM/scr.js
+++ practice/JS/basics/DOM/scr.js
@@ -1,21 +1,51 @@
 function x() {
-	for (var i = 1; i <= 6; i++) {
-		function close(x) {
+	for (let i = 1; i <= 6; i++) {
+		// function close(o) {
 			setTimeout(function() {
-				console.log(x);
-			}, x * 1000);
+				console.log(i);
+			}, i * 1000);
 		}
-		close(i); // creates new copy of x everytime settimeout is called.. 
-	}
+		// close(i); // creates new copy of x everytime settimeout is called.. 
+	// }
 }
 x();
 
-function y() {
-	for (var i = 0 ; i<6 ; i++) {
-		setTimeout(function() {
-		console.log(i);
-		},i*1000);
-	}
-}
 
-y()
+//
+// // function y() {
+// // 	for (var i = 0 ; i<6 ; i++) {
+// // 		setTimeout(function() {
+// // 		console.log(i);
+// // 		},i*1000);
+// // 	}
+// // }
+// // y()
+// //
+// function regular () {
+// 	console.log("regular function executed")
+// }
+// regular()
+// setTimeout(function() {
+// 	console.log("time out executed")
+// },0)
+// //
+// setTimeout(function() {
+// 	console.log("6")
+// }, 6000)
+//
+// setTimeout(function() {
+// 	console.log("5")
+// }, 5000)
+//
+// setTimeout(function() {
+// 	console.log("4")
+// }, 4000)
+// setTimeout(function() {
+// 	console.log("3")
+// }, 3000)
+// setTimeout(function() {
+// 	console.log("2")
+// }, 2000)
+// setTimeout(function() {
+// 	console.log("1")
+// }, 1000)
